2cf2327393f64d275ccf36d106843ae9e89e46f5
Arnie Hsieh <tlhsieh0610@gmail.com>
2023-10-05 13:46:55 +1100

    add basic node

---
+++ basic-Node/Readme.md
@@ -0,0 +1,11 @@
+### Build image
+
+```
+docker build -t "your-image-tag-name" .
+```
+
+### Run container
+
+```
+docker run -p 8080:8080 "your-image-tag-name"
+```
---
+++ basic-Node/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+const port = 8080;
+
+app.get("/", (req, res) => {
+  res.send("Hi There");
+});
+
+app.listen(port, () => {
+  console.log("listening port", port);
+});
